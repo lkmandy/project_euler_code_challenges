8faae1373436b71927dc56f62ffede54d4c15e88
Amanda Shafack <shafack.likhene@gmail.com>
2021-08-26 23:27:34 +0100

    Optimize method

--- multiples_3_or_5/multiples_3_or_5.dart
+++ multiples_3_or_5/multiples_3_or_5.dart
@@ -2,19 +2,20 @@ import 'dart:core';
 
 class Multiple{
 
+  int _sum = 0;
+  
   int sumOfMultiples(){
-    int sum = 0;
     for(int i=1; i<1000; i++){
       if(i%3 == 0 || i%5 == 0){
-        sum = sum + i;
+        _sum += i;
       }
     }
-    return sum;
+    return _sum;
   }
 }
 
 main(){
-  var session1 = Multiple();
+  Multiple session1 = Multiple();
   print('The sum of multiples of 3 and 5 below 1000 = ${session1.sumOfMultiples()}');
 
 }
