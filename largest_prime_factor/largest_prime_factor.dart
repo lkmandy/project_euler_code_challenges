15a3c5981b75bed8c9517ebde56bad7cef03b7a9
Amanda Shafack <shafack.likhene@gmail.com>
2021-09-06 23:55:02 +0100

    Add a package that checks for prime numbers

--- largest_prime_factor/largest_prime_factor.dart
+++ largest_prime_factor/largest_prime_factor.dart
@@ -1,15 +1,20 @@
+import 'dart:math';
+import 'package:ninja_prime/ninja_prime.dart';
 
+largestPrimeFactor() {
+  const int number = 600851475143;
+  List<int> factorSet = [];
+  int maxPrimeNumber=0;
 
-largestPrimeFactor(){
-const int number = 600851475143;
-
-for(int i=2; i<600851475144; i++){
+  for (int i = 2; i < number; i++) {
+    if (number % i == 0 && i.isPrime()) {
+      factorSet.add(i);
+    }
+  }
+  maxPrimeNumber = factorSet.reduce(max);
 
 }
 
-}
-
-main(){
+main() {
   largestPrimeFactor();
-
 }
