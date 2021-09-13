7b9f66c10392343c917303e34390510c2f11dbc0
Amanda Shafack <shafack.likhene@gmail.com>
2021-09-13 22:08:24 +0100

    Find the largest prime factor of a given number

--- largest_prime_factor/largest_prime_factor.dart
+++ largest_prime_factor/largest_prime_factor.dart
@@ -1,20 +1,43 @@
 import 'dart:math';
 import 'package:ninja_prime/ninja_prime.dart';
 
-largestPrimeFactor() {
-  const int number = 600851475143;
-  List<int> factorSet = [];
-  int maxPrimeNumber=0;
+int largestPrimeFactor(int n) {
 
-  for (int i = 2; i < number; i++) {
-    if (number % i == 0 && i.isPrime()) {
-      factorSet.add(i);
+  int maxPrime = -1;
+
+  // Print the number of 2s that divide n
+  while (n % 2 == 0) {
+    maxPrime = 2;
+    n ~/= n / 2;
+  }
+
+  // n must be odd at this point
+  while (n % 3 == 0) {
+    maxPrime = 3;
+    n ~/= 3;
+  }
+
+  // now we have to iterate only for integers
+  // who do not have prime factors 2 and 3
+  for (int i = 5; i <= sqrt(n); i += 6) {
+    while (n % i == 0) {
+      maxPrime = i;
+      n ~/= i;
+    }
+    while (n % (i + 2) == 0) {
+      maxPrime = i + 2;
+      n ~/= i + 2;
     }
   }
-  maxPrimeNumber = factorSet.reduce(max);
 
+  // This condition is to handle the case
+  // when n is a prime number greater than 4
+  if (n > 4) maxPrime = n;
+
+  return maxPrime;
 }
 
 main() {
-  largestPrimeFactor();
+  int number = 600851475143;
+  print(largestPrimeFactor(number));
 }
