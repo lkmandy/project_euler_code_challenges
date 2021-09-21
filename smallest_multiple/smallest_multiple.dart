303a7af5d1212b521c9262ceb470fe77223c6209
Amanda Shafack <shafack.likhene@gmail.com>
2021-09-21 22:54:04 +0100

    Find the samlles factor of the range 1-20

--- smallest_multiple/smallest_multiple.dart
+++ smallest_multiple/smallest_multiple.dart
@@ -1,9 +1,28 @@
+int smallestMultiple() {
+  // 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+  int smallestFactor = 2520;
+  bool found = false;
 
-void smallestMultiple(){
-  // ignore: todo
-  // TODO: Find the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+  while (!found) {
+    // The samllest factor will be a multiple of 2520
+    smallestFactor += 2520;
+    bool isDividable = true;
+
+    // The numbers 1 - 10 are not needed in the for loop -- The numbers 11-20 are divisible by the same numbers as 1-10.
+    // So we start at 11 and end at 20 in the for loop statement.
+    for (int numberRange = 11; numberRange <= 20; numberRange++) {
+      if (smallestFactor % numberRange != 0) {
+        isDividable = false;
+        break;
+      }
+    }
+    if (isDividable) {
+      found = true;
+    }
+  }
+  return smallestFactor;
 }
 
-main(){
-
+main() {
+  print(smallestMultiple());
 }
